26054df8aa22711a62628f1a111bcd2bc4100f55
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-15 20:42:28 +0530

    shift operators

---
+++ shift_operator.java
@@ -0,0 +1,10 @@
+public class Shift_operator {
+    public static void main(String[] args) {
+        int a=5;             //101
+        int b=a<<3;      //101000 =40
+        int c=a>>2;       //1
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
