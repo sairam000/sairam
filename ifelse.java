09ea72492d37ff1565b2b14d64e8adc1996ac089
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-15 21:08:46 +0530

    biggest of 3 numbers

---
+++ ifelse.java
@@ -0,0 +1,24 @@
+class Ifelse {
+
+    public static void main(String[] args) {
+            int a = 6;
+            int b = 7;
+            int c = 5;
+            if(a>b && a>c)
+            {
+                System.out.println("a is greater");
+            }
+            else if(b>c)
+            {
+                System.out.println("a is greater");
+            }
+            else
+            {
+                System.out.println("a is greater");
+            }
+        
+    }
+}
+            
+                    
+    
