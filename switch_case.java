cf1a70ff12474c5a3d08b1e0579e4ee02846bbcc
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 09:29:39 +0530

    using switch case finding a day

---
+++ switch_case.java
@@ -0,0 +1,35 @@
+class Ifelse {
+public static void main(String[] args) {
+       int i=5;
+        switch(i)
+        {
+            case 0:
+                System.out.println("sunday");
+                break;
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wed");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            default :
+                System.out.println("none");
+                break;
+                
+                
+                
+        }
+      }
+}
