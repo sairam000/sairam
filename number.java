a7639edd420a26ab5c75cf4e96d55c13893d971c
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 09:44:39 +0530

    numbers loop

---
+++ number.java
@@ -0,0 +1,14 @@
+class Number {
+public static void main(String[] args) {
+      for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                int k=i+j-1;
+                System.out.print(k +"");
+            }
+            System.out.println("");
+        
+        }
+      }
+}
