26b9900a1fe2e828fc021eb8e40c795a0f5456d5
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 10:28:31 +0530

    finding palindrome number

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+import java .util.Scanner;
+class Palindrome {
+public static void main(String[] args) {
+        int r;
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        int n=sc.nextInt();
+        int t=n;
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        if(t==sum)
+        {
+            System.out.println("palindrome number"+sum);
+         
+        }
+        else
+        {
+            System.out.println("not a palindrome number"+sum);
+         
+        }
+      }
+}
