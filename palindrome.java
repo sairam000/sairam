841593289324bd5b2df8e1a445b05e68f476dc56
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 10:38:11 +0530

    updated armstrong  logic

--- palindrome.java
+++ palindrome.java
@@ -10,17 +10,17 @@ public static void main(String[] args) {
         while(n>0)
         {
             r=n%10;
-            sum=sum*10+r;
+            sum=sum+r*r*r;
             n=n/10;
         }
         if(t==sum)
         {
-            System.out.println("palindrome number"+sum);
+            System.out.println("armstrong  number"+sum);
          
         }
         else
         {
-            System.out.println("not a palindrome number"+sum);
+            System.out.println("not a armstrong number"+sum);
          
         }
       }
