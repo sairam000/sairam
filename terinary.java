43ebcdbd3688fbdb2cfeb90544d4ec5ca26dd6c9
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 09:22:44 +0530

    terinary operator

---
+++ terinary.java
@@ -0,0 +1,10 @@
+public class terinary{
+ public static void main(String[] args) {
+            int i=5;
+            int j=i==5?4:6;
+            {
+                System.out.println(j);
+            }
+        
+    }
+}
