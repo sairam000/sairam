c9e2cb5a4931ab8dce8adf857a6253cee22a6a88
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-16 09:49:19 +0530

    adding another feature

--- number.java
+++ number.java
@@ -4,9 +4,18 @@ public static void main(String[] args) {
         {
             for(int j=1;j<=5;j++)
             {
+                
                 int k=i+j-1;
-                System.out.print(k +"");
-            }
+                if(k>5)
+                {
+                System.out.print(k-5 +"");
+                }
+                else
+                {
+                    System.out.print(k +"");
+                
+                }
+             }
             System.out.println("");
         
         }
