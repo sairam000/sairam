7164524397d84c527e7cd4c64e655684d59a7305
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-20 16:02:09 +0530

    user input buffered reader

---
+++ buffer.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class buffer
+{
+public static void main(String []args)throws Exception
+{
+BufferedReader bin=new BufferedReader(new InputStreamReader(System.in));
+String n=(bin.readLine());
+System.out.println(n);
+}
+}
