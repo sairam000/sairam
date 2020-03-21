1d7ceb1d93c4a19b5a4cc89aaa55a1839ed4dd36
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-21 16:03:48 +0530

    multithread using runnable interface

---
+++ multirunnable.java
@@ -0,0 +1,50 @@
+class sai implements Runnable
+{
+public void run()
+{
+for(int i=1;i<=3;i++)
+{
+System.out.println("sai");
+try
+{
+Thread.sleep(1000);
+}
+catch(Exception e){
+}
+}
+}
+}
+class ram implements Runnable
+{
+public void run()
+{
+for(int i=1;i<=3;i++)
+{
+System.out.println("ram");
+try
+{
+Thread.sleep(1000);
+}
+catch(Exception e){
+}
+}
+}
+}
+class Myclass
+{
+public static void main(String []args)
+{
+Runnable r1=new sai();
+Runnable r2=new ram();
+Thread t1=new Thread(r1);
+Thread t2=new Thread(r2);
+t1.start();
+try
+{
+Thread.sleep(1000);
+}
+catch(Exception e){
+}
+t2.start();
+}
+}
