d1613c8412ec64272da3c46c1769612a547939df
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-21 16:17:16 +0530

    string buffer example

---
+++ stringBuffer.java
@@ -0,0 +1,11 @@
+class stringBuf
+{
+public static void main(String []args)
+{
+StringBuffer s=new StringBuffer("sai ");
+s.append("is ");
+s.append(" handsome");
+s.replace(0,3,"ram");
+System.out.println(s);
+}
+}
