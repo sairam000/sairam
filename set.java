480862a53ae9b126415b701ff62c57f9b11a0f02
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-22 10:47:55 +0530

    set interface

---
+++ set.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class set1
+{
+public static void main(String []args)
+{
+Set<Integer> s=new HashSet<>();
+System.out.println(s.add(5));
+System.out.println(s.add(6));
+System.out.println(s.add(7));
+System.out.println(s.add(6));
+System.out.println(s);
+}
+}
