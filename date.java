518fbd5679e585838210d76b709b239be0559b9e
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-03-22 20:31:13 +0530

    date example

---
+++ date.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+class random
+{
+public static void main(String []args)
+{
+long s=System.currentTimeMillis()-1000*60*60*24;
+Date d=new Date();
+System.out.println(d);
+}
+}
