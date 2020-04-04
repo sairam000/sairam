8541a083da8d15de95ffdd018e84a69f4e445ced
sairam <sairammullapudi000@gmail.com>
2020-04-04 22:14:58 +0530

    spring demo

---
+++ spring demo/src/firstspring/Rectangle.java
@@ -0,0 +1,39 @@
+package firstspring;
+
+import java.util.List;
+
+public class Rectangle {
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	public Point getPointA() {
+		return pointA;
+	}
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+	public Point getPointB() {
+		return pointB;
+	}
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+	public void draw()
+	{
+			
+		System.out.println("point A =(" + getPointA().getX() + ","+getPointA().getY() + ")");
+		System.out.println("point B =(" + getPointB().getX() + ","+getPointB().getY() + ")");
+		System.out.println("point C =(" + getPointC().getX() +","+getPointC().getY() + ")");
+	}
+	
+	
+	
+
+}
