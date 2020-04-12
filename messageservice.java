aefa74832b7ba4a0e31e81a22261ed71a972bca6
sairam000 <62096416+sairam000@users.noreply.github.com>
2020-04-12 11:23:24 +0530

    small changes

--- messageservice.java
+++ messageservice.java
@@ -3,10 +3,20 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import model.Message;
-
+@Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class messageservice {
 	public List<Message> getallmessages()
 	{
@@ -19,15 +29,29 @@ public class messageservice {
 		return l;
 			}
     @POST
-	public  Message addMessage(Message message) {
+	public static  Message addMessage(Message message) {
 		// TODO Auto-generated method stub
 		return messageservice.addMessage(message);
 	}
-
+    @GET
 	public static Message getmessage(long id) {
 		// TODO Auto-generated method stub
-		return null;
-	}.
+		return messageservice.getmessage(id);
+	}
+    @PUT
+    @Path("messageId")
+	public static Message updatemessage(@PathParam("messageId") long id, Message message) 
+	{
+		// TODO Auto-generated method stub
+		message.setId(id);
+		return messageservice.updatemessage(id, message);
+	}
+    @DELETE
+	public static Message deletemessage(Message message) {
+		// TODO Auto-generated method stub
+		return messageservice.deletemessage(message);
+	}
+    
 
 	
 
--- messenger.java
+++ messenger.java
@@ -3,8 +3,10 @@ package resource;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,39 +14,47 @@ import javax.ws.rs.core.MediaType;
 
 import model.Message;
 import service.messageservice;
-
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class messenger {
 	messageservice Messageserv=new messageservice();
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getmessage()
 	{
 		return Messageserv.getallmessages();
 	}
 	
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message)
 	{
-		return Messageservice.addMessage(message);
+		return Messageserv.addMessage(message);
+	}
+	@PUT
+	@Path("/(messageId)")
+	public Message updatemessage(@PathParam("(messageId)") long id,Message message)
+	{
+		message.setId(id);
+		return Messageserv.updatemessage(message);
+	}
+	@DELETE
+	@Path("/messageId")
+	public Message deleteMessage(@PathParam("messageId")long id)
+	{
+		return Messageserv.deleteMessage(id);
 	}
 	@GET
-	@PathParam("/(messageId)")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message updateMessage(Message message)
+	@Path("/messageId")
+	public Message getmessage(@PathParam("messageId")long id)
 	{
-		return 
+		return Messageserv.getmessage(id);
 	}
 	
-	@GET
-	@PathParam("/(messageId)")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getmessage(@PathParam("messageId") long id)
-	{
-		return messageservice.getmessage(id);
-	}
+	
+	
+	
+	
+	
 
 
 }
